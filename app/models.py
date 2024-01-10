d7931699acff1908de4a85b3bb6aca6ca536bd1d
Balaji7077 <Balajisabat6@gmail.com>
2024-01-10 15:13:33 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class School(models.Model):
+    Sname=models.CharField(max_length=100)
+    Sprincipal=models.CharField(max_length=100)
+    Slocation=models.CharField(max_length=100)
+    email=models.EmailField(default='hai@gmail.com')
+    renteremail=models.EmailField(default='hai@gmail.com')
+
+    def _str_(self):
+        return self.Sname
